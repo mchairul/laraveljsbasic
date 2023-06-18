fb1b8f5717f2c11825a6cedb36631a37a0375af4
Muhammad Chairul <email.muhammad.chairul@gmail.com>
2023-06-18 13:19:12 +0700

    Create db.sql

---
+++ db.sql
@@ -0,0 +1,50 @@
+-- Adminer 4.8.0 MySQL 5.7.34 dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+CREATE TABLE `chats` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `id_conversation` bigint(20) NOT NULL,
+  `tanggal` datetime NOT NULL,
+  `user` varchar(255) NOT NULL,
+  `chat` varchar(255) NOT NULL,
+  `is_processed` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_conversation` (`id_conversation`),
+  CONSTRAINT `chats_ibfk_1` FOREIGN KEY (`id_conversation`) REFERENCES `conversations` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `conversations` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user1` varchar(255) NOT NULL,
+  `user2` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `sales` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `tgl_penjualan` datetime NOT NULL,
+  `nama_produk` varchar(255) NOT NULL,
+  `quantity` int(4) NOT NULL,
+  `harga` bigint(20) NOT NULL,
+  `total` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `tickets` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `tanggal` datetime NOT NULL,
+  `user` varchar(255) NOT NULL,
+  `description` longtext NOT NULL,
+  `attachment` longtext,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- 2023-06-18 06:18:54
